d5eef187098c9525d4c7e631246dbd752672b590
Vinivy Gabriel <vinivygabriel8@gmail.com>
2024-02-06 23:01:22 -0300

    Concluido

--- Script.js
+++ Script.js
@@ -1,5 +1,5 @@
 let interactionsImg = document.querySelectorAll('.carteira, .quadro, .Sell')
-let boxes = document.querySelectorAll('.huge, .quality, .resource, .community' )
+let boxes = document.querySelectorAll('.huge, .quality, .resource, .community')
 let exemperfil = document.querySelectorAll('.Step, .Andry, .Zaid, .Laila')
 let infosDireict = document.querySelectorAll('.antiondireitaOne , .antiondireitaTwo')
 // modelo de observar o conteudo
@@ -8,9 +8,7 @@ const Myobserv = new IntersectionObserver((entries) => {
         if (entry.isIntersecting) {
           entry.target.classList.remove('carteira', 'quadro', 'Sell')
           entry.target.classList.add('show')
-        } else {
-          entry.target.classList.remove('show')
-        }
+        } 
     })
 })
 
@@ -25,8 +23,6 @@ const observ = new IntersectionObserver((Puxando) => {
       //primeiro vamos remover e dps adicionar para n da erro de codigo
       atribuindo.target.classList.remove('huge', 'quality', 'resource', 'community')
       atribuindo.target.classList.add('Transitiontop')
-    }else {
-      atribuindo.target.classList.remove('Transitiontop')      
     }
   })
 })
@@ -40,9 +36,7 @@ const Artes = new IntersectionObserver((Avatares) => {
     if(AVAs.isIntersecting){
       AVAs.target.classList.remove('Step', 'Andry', 'Zaid', 'Laila')
       AVAs.target.classList.add('Transitiontop')
-    }else{
-      AVAs.target.classList.remove('Transitiontop')
-    }
+    } 
   })
 })
 
@@ -55,8 +49,6 @@ const description = new IntersectionObserver((Desc) => {
     if(glass.isIntersecting) {
       glass.target.classList.remove('antiondireitaOne', 'antiondireitaTwo');
       glass.target.classList.add('show')
-    }else {
-      glass.target.classList.remove('show')
     }
   })
 })
@@ -65,6 +57,7 @@ const description = new IntersectionObserver((Desc) => {
   description.observe(Yes)
 })
 
+const adies = document.querySelector('#AddMais')
 function toggleDarkMode() {
   let body = document.querySelector('body')
   let Home = document.querySelector('#Home')
@@ -75,13 +68,6 @@ function toggleDarkMode() {
   let CoinBase = document.querySelector('.CoinBase')
   let TrustWallet = document.querySelector('.TrustWallet')
   let Exodus = document.querySelector('.Exodus')
-  /*redes */
-  let Twi = document.querySelector('ion-icon')
-  let Ds = document.querySelector('#Discord')
-  let Ins = document.querySelector('#Instagram')
-  let Wall = document.querySelector('#Wallet')
-  let Sn = document.querySelector('#Sun')
-
 
   if (!body.classList.contains('Dark')) {
     body.classList.add('Dark')
@@ -94,12 +80,6 @@ function toggleDarkMode() {
     TrustWallet.setAttribute('src', './Assets/Componentes/Marcas/TrustWalletDark.svg')
     Exodus.setAttribute('src', './Assets/Componentes/Marcas/EXODUSDark.svg')
 
-    Twi.style.color = '#ffffff'
-    Ds.style.color = '#ffffff'
-    Ins.style.color = '#ffffff' 
-    Wall.style.color = '#ffffff'
-    Sn.style.color = 'black'
-
   } else {
     body.classList.remove('Dark')
     Home.style.background = 'url(./Assets/Background/BackgroundHome.jpg)'
@@ -110,12 +90,6 @@ function toggleDarkMode() {
     CoinBase.setAttribute('src', './Assets/Componentes/Marcas/CoinBase.svg')
     TrustWallet.setAttribute('src', './Assets/Componentes/Marcas/TrustWallet.svg')
     Exodus.setAttribute('src', './Assets/Componentes/Marcas/EXODUS.svg')
-
-   Twi.style.color = 'black'
-   Ds.style.color = 'black'
-   Ins.style.color = 'black'
-   Wall.style.color = 'black'
-   Sn.style.color = '#ffffff'
   }
 }
  
